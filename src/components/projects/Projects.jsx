76666dc73d1b37f79a6bd3bdc2a3dc1ffbf0eb6b
Andres <andresv@grava.io>
2023-02-28 01:35:26 -0300

    save changes

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,15 +2,6 @@ import './styles.scss';
 export default function Projects(){
     return(
         <div className="projects">
-            <div className="card">
-                <h1>card</h1>
-            </div>
-            <div className="card">
-                <h1>card</h1>
-            </div>
-            <div className="card">
-                <h1>card</h1>
-            </div>
         </div>
     )
 }
