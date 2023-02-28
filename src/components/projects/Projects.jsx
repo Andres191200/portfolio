346595015c1855f5e78a5787bf5d978f25c364e8
Andres <andresv@grava.io>
2023-02-28 01:45:14 -0300

    save changes

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,10 @@
 import './styles.scss';
 export default function Projects(){
     return(
-        <div className="projects">
-        </div>
+        <>
+            <div className="no-responsive-message"><p>Isn't responsive...</p><p>yet...</p></div>
+            <div className="projects">
+            </div>
+        </>
     )
 }
